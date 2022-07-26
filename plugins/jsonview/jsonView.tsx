712b4ae8dee81615dc59b4f081a3fc5d92db8f7e
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-07-26 16:11:59 -0700

    step 3: portable text props, prettier switch

--- plugins/jsonview/jsonView.tsx
+++ plugins/jsonview/jsonView.tsx
@@ -1,14 +1,25 @@
-import {Card, Code} from '@sanity/ui'
-import {InputProps} from 'sanity'
+import {Code, Inline, Stack, Switch, Text} from '@sanity/ui'
+import {useState} from 'react'
+import {PortableTextInput, PortableTextInputProps} from 'sanity/form'
 
 /**
- * - Typed input props! Hooray!
- * - Use syntax highlighting through @sanity/ui's `<Code />`
+ * - Replaced InputProps with PortableTextInputProps
+ * - Conditionally render input or JSON based on switch state
  */
-export function JsonView(props: InputProps) {
+export function JsonView(props: PortableTextInputProps) {
+  const [showJson, setShowJson] = useState(false)
   return (
-    <Card>
-      <Code language="json">{JSON.stringify(props.value, null, 2)}</Code>
-    </Card>
+    <Stack space={2}>
+      <Inline space={2} style={{textAlign: 'right'}}>
+        <Switch checked={showJson} onChange={() => setShowJson((current) => !current)} />
+        <Text>Show JSON</Text>
+      </Inline>
+
+      {showJson ? (
+        <Code language="json">{JSON.stringify(props.value, null, 2)}</Code>
+      ) : (
+        <PortableTextInput {...props} />
+      )}
+    </Stack>
   )
 }
