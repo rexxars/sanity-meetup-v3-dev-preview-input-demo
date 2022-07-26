d38c0633cfc58636b055047573432948747d13f5
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-07-26 16:29:08 -0700

    step 4: only render json view if developer

--- plugins/jsonview/jsonView.tsx
+++ plugins/jsonview/jsonView.tsx
@@ -1,13 +1,20 @@
 import {Code, Inline, Stack, Switch, Text} from '@sanity/ui'
 import {useState} from 'react'
+import {useCurrentUser} from 'sanity'
 import {PortableTextInput, PortableTextInputProps} from 'sanity/form'
 
 /**
- * - Replaced InputProps with PortableTextInputProps
- * - Conditionally render input or JSON based on switch state
+ * - Only render JSON option if users role is a developer
  */
 export function JsonView(props: PortableTextInputProps) {
   const [showJson, setShowJson] = useState(false)
+  const user = useCurrentUser()
+  const userIsDeveloper = user?.roles.some((role) => role.name === 'developer')
+
+  if (!userIsDeveloper) {
+    return <PortableTextInput {...props} />
+  }
+
   return (
     <Stack space={2}>
       <Inline space={2} style={{textAlign: 'right'}}>
